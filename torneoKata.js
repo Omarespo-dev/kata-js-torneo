81e7a6d788070ccf40e617ccf3b5405ee1a14628
Omarespo-dev <omaresposito21@gmail.com>
2025-09-05 19:20:40 +0200

    Aggiunta datafighters e weapons

---
+++ torneoKata.js
@@ -0,0 +1,8 @@
+//Importo i dati dei fighters e delle armi
+import { fighters } from "./dataFighters.js"
+import { weapons } from "./dataWeapons.js"
+
+
+//log di debug
+console.log(fighters)
+console.log(weapons)
