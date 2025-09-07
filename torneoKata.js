abfde5e14f75a9084a6b8ca8794ebac5b92aac2c
Omarespo-dev <omaresposito21@gmail.com>
2025-09-07 13:05:31 +0200

    fase 2 completata

--- torneoKata.js
+++ torneoKata.js
@@ -22,18 +22,42 @@ function casualWeapon(arrFighters) {
         const casualWeapon = weapon[randomIndex];
 
         // rimuovo l’arma così non è più disponibile
-        weapon.splice(randomIndex , 1);
+        weapon.splice(randomIndex, 1);
 
 
         //ritorno aggiunta armi
         return { ...figth, weaponName: casualWeapon.name, weaponPower: casualWeapon.power }
     })
 
-   
-    
+
+
     return addWeapon
 
 }
 
 console.log(casualWeapon(fighters))
 // ⚠️ Una volta scelta, un'arma non sarà più disponibile per i successivi combattenti.
+
+
+
+
+
+//### Fase 2 - 💪 Allenamento
+// Ogni combattente si sottoporrà a un allenamento che incrementerà (o forse no) la sua potenza.
+
+const arrObjCompleto = casualWeapon(fighters)
+// console.log(arrObjCompleto);
+
+const allenamento = arrObjCompleto.map(fighter => {
+    
+    //potenza casuale incremento
+    const incrementoPotenza = Math.floor(Math.random() * 100) + 1;
+
+    //Copia di fighter e modifico power
+    return {...fighter , power: fighter.power * incrementoPotenza}
+
+})
+
+console.log(allenamento);
+
+// La potenza verrà moltiplicata per un numero casuale tra **1 e 100**.
